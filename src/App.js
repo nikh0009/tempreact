ca0be1f153de61a776d7d69b0b074ea98ef20595
Nikhil Tripathy <nikhilt231@gmail.com>
2019-06-21 10:57:09 +0530

    reduced README and basic routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React,{Component} from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 import './App.css';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 class App extends Component{
   state ={
     todos:[
       {
-        id:1,
+        id:uuid.v4(),
         title:"Get up!",
         status:false
       },
       {
-        id:2,
+        id:uuid.v4(),
         title:"Sleep.. Again!",
         status:false
       },
       {
-        id:3,
+        id:uuid.v4(),
         title:"Get Ready for work...",
         status:false
       }
@@ -38,16 +40,30 @@ class App extends Component{
     this.setState ({todos : [...this.state.todos.filter(todo => todo.id !== id)]})
   }
 
+  //AddTodo function
+  addTodo = (title) =>{
+    const newTodo = {
+      id:uuid.v4(),
+      title,
+      status: false
+    }
+    this.setState({
+      todos: [...this.state.todos,newTodo]
+    })
+  }
+
 
   render(){
     return (
+      <Router>
       <div className="App">
         <div className="container" >
           <Header />
-          <AddTodo />
+          <AddTodo addTodo= {this.addTodo} />
           <Todos todoList={this.state.todos} markComplete= {this.markComplete} delTodo= {this.delTodo}/>
         </div>
       </div>
+      </Router>
     );
   }
 }
